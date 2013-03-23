200753452d4ca21031912d432347d64ffb15652a
dima117a <dima117a@97b3ecbd-a1e6-0508-9bb6-6bfb78539658>
2013-03-23 08:49:40 +0000

    - объектная модель для конфигурирования интерфейса;

---
+++ trunk/ThinkingHome.NooLite.Web/App_Code/Configuration/ControlAction.cs
@@ -0,0 +1,9 @@
+﻿namespace ThinkingHome.NooLite.Web.Configuration
+{
+	public class ControlAction
+	{
+		public byte Channel { get; set; }
+
+		public byte? Level { get; set; }
+	}
+}
---
+++ trunk/ThinkingHome.NooLite.Web/App_Code/Configuration/ControlSection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ThinkingHome.NooLite.Web.Configuration
+{
+	public class ControlSection
+	{
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public IEnumerable<Control> Controls { get; set; }
+	}
+}
