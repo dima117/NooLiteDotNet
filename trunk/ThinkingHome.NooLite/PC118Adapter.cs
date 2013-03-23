70cbc9b7dd425a756af3df1ae5db737e616cb728
dima117a <dima117a@97b3ecbd-a1e6-0508-9bb6-6bfb78539658>
2013-03-23 07:46:05 +0000

    - адаптер noolite;

---
+++ trunk/ThinkingHome.NooLite/PC118Adapter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using HidLibrary;
+
+namespace ThinkingHome.NooLite
+{
+	public class PC118Adapter : IDisposable
+    {
+        private const int VENDOR_ID = 0x16C0;
+        private const int PRODUCT_ID = 0x05DF;
+
+        private HidDevice device;
+
+		public bool IsConnected
+		{
+			get { return device != null && device.IsConnected; }
+		}
+
+		public bool OpenDevice()
+        {
+            device = HidDevices.Enumerate(VENDOR_ID, PRODUCT_ID).FirstOrDefault();
+
+	        if (device != null)
+	        {
+				device.OpenDevice();
+				device.MonitorDeviceEvents = true;
+
+		        return true;
+	        }
+
+	        return false;
+        }
+
+        public void SendCommand(Pc118Command cmd, byte channel, byte level = 0)
+        {
+            var data = new byte[] { 0x00, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+            data[2] = (byte)cmd;
+            data[5] = channel;
+
+            if (cmd == Pc118Command.SetLevel)
+            {
+                data[3] = 0x01;
+                data[6] = level;
+            }
+
+            device.WriteFeatureData(data);
+            //device.Write(data);
+        }
+
+        public void Dispose()
+        {
+            device.Dispose();
+        }
+    }
+}
---
+++ trunk/ThinkingHome.NooLite/Pc118Command.cs
@@ -0,0 +1,12 @@
+﻿namespace ThinkingHome.NooLite
+{
+	public enum Pc118Command : byte
+	{
+		On = 0x02,
+		Off = 0x00,
+		Switch = 0x04,
+		SetLevel = 0x06,
+		Bind = 0x0f,
+		UnBind = 0x09,
+	}
+}
