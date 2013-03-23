d9e34ba833326b443a199f40569f4c2e6acbc2ed
dima117 <dima117a@gmail.com>
2013-03-23 11:32:29 +0000

    - конфигурационная секция с коллекцией страниц приложения; - картинки для jquery-mobile; - отображение в броузере списка страниц из конфига и переход по страницам;

---
+++ ThinkingHome.NooLite.Web/App_Code/Configuration/NooLiteConfigurationSection.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace ThinkingHome.NooLite.Web.Configuration
+{
+	public class NooLiteConfigurationSection : ConfigurationSection
+	{
+		private static readonly ConfigurationProperty propControlPages = new ConfigurationProperty(null, typeof(ControlPageCollection), null, ConfigurationPropertyOptions.IsDefaultCollection);
+
+		[ConfigurationProperty("", IsDefaultCollection = true)]
+		public ControlPageCollection ControlPages
+		{
+			get
+			{
+				return (ControlPageCollection)base[propControlPages];
+			}
+		}
+	}
+}
