1b6f7ed42eaa48f25f55505553c608e5981c10c0
dima117 <dima117a@gmail.com>
2013-05-17 16:38:20 +0000

    - модель рабочих данных редактора конфига;

--- ThinkingHome.NooLite.Web.Configurator/Form1.cs
+++ ThinkingHome.NooLite.Web.Configurator/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace ThinkingHome.NooLite.Web.Configurator
 {
@@ -14,6 +16,17 @@ namespace ThinkingHome.NooLite.Web.Configurator
 		public Form1()
 		{
 			InitializeComponent();
+
+			LoadConfig();
+		}
+
+		private void LoadConfig()
+		{
+			var mySerializer = new XmlSerializer(typeof(NooLiteConfiguration));
+
+			var myFileStream = new FileStream("NooLite.config", FileMode.Open);
+			// Call the Deserialize method and cast to the object type.
+			var obj = mySerializer.Deserialize(myFileStream) as NooLiteConfiguration;
 
 		}
 	}
