8c6d4a7a6393ede1dff8debe85180aee3fc38211
dima117a <dima117a@97b3ecbd-a1e6-0508-9bb6-6bfb78539658>
2013-05-17 16:38:20 +0000

    - модель рабочих данных редактора конфига;

---
+++ trunk/ThinkingHome.NooLite.Web.Configurator/NooLiteConfiguration.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ThinkingHome.NooLite.Web.Configurator
+{
+	[XmlRoot("nooLiteConfiguration")]
+	public class NooLiteConfiguration
+	{
+		[XmlAttribute("debug")]
+		public bool Debug { get; set; }
+
+		[XmlElement("page")]
+		public List<NooliteControlPage> Pages { get; set; }
+	}
+
+	public class NooliteControlPage
+	{
+		[XmlAttribute("id")]
+		public string Id { get; set; }
+
+		[XmlAttribute("title")]
+		public string Title { get; set; }
+
+		[XmlAttribute("description")]
+		public string Description { get; set; }
+
+		[XmlElement("control")]
+		public List<NooliteControl> Controls { get; set; }
+
+	}
+	public class NooliteControl
+	{
+		[XmlAttribute("id")]
+		public string Id { get; set; }
+
+		[XmlAttribute("displayText")]
+		public string DisplayText { get; set; }
+
+		[XmlAttribute("type")]
+		public ControlType ControlType { get; set; }
+
+		[XmlAttribute("level")]
+		public byte Level { get; set; }
+
+		[XmlElement("channel")]
+		public List<NooliteChannelAction> Actions { get; set; }
+
+	}
+
+	public class NooliteChannelAction
+	{
+		[XmlAttribute("id")]
+		public byte ChannelId { get; set; }
+
+		[XmlAttribute("level")]
+		public string LevelText { get; set; }
+
+		[XmlIgnore]
+		public byte? Level
+		{
+			get
+			{
+				byte result;
+				return byte.TryParse(LevelText, out result) ? (byte?)result : null;
+			}
+			set
+			{
+				LevelText = value.HasValue ? value.ToString() : null;
+			}
+		}
+	}
+}
