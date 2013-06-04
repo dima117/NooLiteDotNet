5ca48e92f3834ff8fcd6b277597d8fdaf6573bcf
dima117a <dima117a@97b3ecbd-a1e6-0508-9bb6-6bfb78539658>
2013-06-04 15:39:30 +0000

    - форма редактора действий в каналах; - отображение списка действий в каналах;

---
+++ trunk/ThinkingHome.NooLite.Web.Configurator/ChannelActionEditorForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ThinkingHome.NooLite.Web.Configurator
+{
+	public partial class ChannelActionEditorForm : Form
+	{
+		public ChannelActionEditorForm()
+		{
+			InitializeComponent();
+		}
+
+		public ChannelActionEditorForm(int[] usedIds)
+		{
+			
+		}
+
+		private void cbSpecificLevel_CheckedChanged(object sender, EventArgs e)
+		{
+			trbLevel.Enabled = lblLevel.Enabled = cbSpecificLevel.Checked;
+		}
+	}
+}
--- trunk/ThinkingHome.NooLite.Web.Configurator/ControlEditorForm.Designer.cs
+++ trunk/ThinkingHome.NooLite.Web.Configurator/ControlEditorForm.Designer.cs
@@ -40,15 +40,21 @@
 			this.btnSave = new System.Windows.Forms.Button();
 			this.label4 = new System.Windows.Forms.Label();
 			this.trbDefaultLevel = new System.Windows.Forms.TrackBar();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.lbChannelActions = new System.Windows.Forms.ListBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnEdit = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.trbDefaultLevel)).BeginInit();
+			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tbTitle
 			// 
 			this.tbTitle.Location = new System.Drawing.Point(142, 38);
 			this.tbTitle.Name = "tbTitle";
-			this.tbTitle.Size = new System.Drawing.Size(209, 20);
+			this.tbTitle.Size = new System.Drawing.Size(212, 20);
 			this.tbTitle.TabIndex = 20;
 			// 
 			// tbIdentifier
@@ -106,19 +112,21 @@
 			// 
 			// btnCancel
 			// 
+			this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(339, 227);
+			this.btnCancel.Location = new System.Drawing.Point(266, 291);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.Size = new System.Drawing.Size(88, 23);
 			this.btnCancel.TabIndex = 260;
 			this.btnCancel.Text = "Отмена";
 			this.btnCancel.UseVisualStyleBackColor = true;
 			// 
 			// btnSave
 			// 
-			this.btnSave.Location = new System.Drawing.Point(258, 227);
+			this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSave.Location = new System.Drawing.Point(172, 291);
 			this.btnSave.Name = "btnSave";
-			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.Size = new System.Drawing.Size(88, 23);
 			this.btnSave.TabIndex = 250;
 			this.btnSave.Text = "Сохранить";
 			this.btnSave.UseVisualStyleBackColor = true;
@@ -136,10 +144,65 @@
 			// trbDefaultLevel
 			// 
 			this.trbDefaultLevel.Location = new System.Drawing.Point(142, 91);
+			this.trbDefaultLevel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
 			this.trbDefaultLevel.Name = "trbDefaultLevel";
-			this.trbDefaultLevel.Size = new System.Drawing.Size(209, 45);
+			this.trbDefaultLevel.Size = new System.Drawing.Size(212, 45);
 			this.trbDefaultLevel.SmallChange = 10;
 			this.trbDefaultLevel.TabIndex = 40;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.btnAdd);
+			this.groupBox1.Controls.Add(this.btnEdit);
+			this.groupBox1.Controls.Add(this.btnDelete);
+			this.groupBox1.Controls.Add(this.lbChannelActions);
+			this.groupBox1.Location = new System.Drawing.Point(15, 136);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(339, 149);
+			this.groupBox1.TabIndex = 261;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Уровень яркости в каналах";
+			// 
+			// lbChannelActions
+			// 
+			this.lbChannelActions.DisplayMember = "ConfiguratorUiDisplayText";
+			this.lbChannelActions.FormattingEnabled = true;
+			this.lbChannelActions.Location = new System.Drawing.Point(6, 48);
+			this.lbChannelActions.MultiColumn = true;
+			this.lbChannelActions.Name = "lbChannelActions";
+			this.lbChannelActions.Size = new System.Drawing.Size(327, 95);
+			this.lbChannelActions.TabIndex = 262;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(6, 19);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 262;
+			this.btnAdd.Text = "Добавить";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnEdit
+			// 
+			this.btnEdit.Location = new System.Drawing.Point(87, 19);
+			this.btnEdit.Name = "btnEdit";
+			this.btnEdit.Size = new System.Drawing.Size(75, 23);
+			this.btnEdit.TabIndex = 263;
+			this.btnEdit.Text = "Изменить";
+			this.btnEdit.UseVisualStyleBackColor = true;
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Location = new System.Drawing.Point(168, 19);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(75, 23);
+			this.btnDelete.TabIndex = 264;
+			this.btnDelete.Text = "Удалить";
+			this.btnDelete.UseVisualStyleBackColor = true;
 			// 
 			// ControlEditorForm
 			// 
@@ -147,7 +210,8 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.btnCancel;
-			this.ClientSize = new System.Drawing.Size(426, 262);
+			this.ClientSize = new System.Drawing.Size(366, 326);
+			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.trbDefaultLevel);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.btnSave);
@@ -167,6 +231,7 @@
 			this.Text = "Редактор элемента управления";
 			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.trbDefaultLevel)).EndInit();
+			this.groupBox1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -185,5 +250,10 @@
 		private System.Windows.Forms.Button btnCancel;
 		private System.Windows.Forms.TrackBar trbDefaultLevel;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ListBox lbChannelActions;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnEdit;
+		private System.Windows.Forms.Button btnDelete;
 	}
 }
