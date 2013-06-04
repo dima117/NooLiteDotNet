f2adb6879223b88cc630dcfd7e3c0b999cb22c57
dima117 <dima117a@gmail.com>
2013-06-04 16:42:50 +0000

    - отображение формы редактора яркости в канале;

--- ThinkingHome.NooLite.Web.Configurator/ChannelActionEditorForm.cs
+++ ThinkingHome.NooLite.Web.Configurator/ChannelActionEditorForm.cs
@@ -11,15 +11,58 @@ namespace ThinkingHome.NooLite.Web.Configurator
 {
 	public partial class ChannelActionEditorForm : Form
 	{
+		public readonly NooliteChannelAction ChannelAction = new NooliteChannelAction();
+		private readonly byte[] usedIds = new byte[0];
+		private const byte MAX_CHANNEL_COUNT = 32;
+
 		public ChannelActionEditorForm()
 		{
 			InitializeComponent();
 		}
 
-		public ChannelActionEditorForm(int[] usedIds)
+		public ChannelActionEditorForm(byte[] ids)
 		{
-			
+			InitializeComponent();
+			usedIds = ids;
+
+			FillFormControls(ChannelAction);
 		}
+
+		public ChannelActionEditorForm(byte[] ids, NooliteChannelAction action)
+		{
+			InitializeComponent();
+			ChannelAction = action;
+			usedIds = ids;
+
+			FillFormControls(ChannelAction);
+		}
+
+		private void FillFormControls(NooliteChannelAction action)
+		{
+			for (byte i = 0; i < MAX_CHANNEL_COUNT; i++)
+			{
+				if (!usedIds.Contains(i) || action.ChannelId == i)
+				{
+					ddlChannelIds.Items.Add(i);
+				}
+			}
+
+			if (action.ChannelId > 0 && action.ChannelId < MAX_CHANNEL_COUNT)
+			{
+				ddlChannelIds.SelectedIndex = 
+					ddlChannelIds.FindStringExact(action.ChannelId.ToString());
+			}
+
+			cbSpecificLevel.Checked = action.Level.HasValue;
+
+			if (action.Level.HasValue)
+			{
+				var l = action.Level.Value;
+				var level = l > 100 ? 100 : l;
+				trbLevel.Value = level / 10;
+			}
+		}
+
 
 		private void cbSpecificLevel_CheckedChanged(object sender, EventArgs e)
 		{
