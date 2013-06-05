9d277f641428f8db7c4e57e2d396951e9a0d70da
dima117 <dima117a@gmail.com>
2013-06-05 15:48:12 +0000

    - шаблон лицензионнго соглашения для общего инсталлятора; - сохранение конфига без xmlns;

--- ThinkingHome.NooLite.Web.Configurator/NooLiteConfiguration.cs
+++ ThinkingHome.NooLite.Web.Configurator/NooLiteConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Xml.Serialization;
 using ThinkingHome.NooLite.Web.Configurator.Annotations;
 
