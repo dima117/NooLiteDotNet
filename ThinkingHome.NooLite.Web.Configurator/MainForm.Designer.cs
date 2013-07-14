ebd7fc4ad0c6d6b1f7aa6f720fb5c7f8621a505d
dima117 <dima117a@gmail.com>
2013-07-14 09:09:47 +0000

    - пиктограмма для конфигуратора;

--- ThinkingHome.NooLite.Web.Configurator/MainForm.Designer.cs
+++ ThinkingHome.NooLite.Web.Configurator/MainForm.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.btnSave = new System.Windows.Forms.Button();
 			this.btnCancel = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
@@ -181,6 +182,7 @@
 			this.Controls.Add(this.btnCancel);
 			this.Controls.Add(this.btnSave);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
