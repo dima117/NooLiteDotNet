439406ae082c610d23bfb2aa32ce6fa80e59a8ef
dima117 <dima117a@gmail.com>
2013-07-17 17:11:37 +0000

    - отмена регистрации веб-приложения;

--- ThinkingHome.NooLite.Install.CustomActions/CustomAction.cs
+++ ThinkingHome.NooLite.Install.CustomActions/CustomAction.cs
@@ -12,31 +12,52 @@ namespace ThinkingHome.NooLite.Install.CustomActions
 		public static ActionResult RegisterApplication(Session session)
 		{
 			Debugger.Launch();
+			session.Log("Begin configure web application");
 
 			try
 			{
+				// parameters
 				Guid appId = new Guid(session["APP_ID"]);
 				ushort port = ushort.Parse(session["APP_PORT"]);
 				string path = session["APP_PATH"];
 
-				session.Log("Begin Configure NooLite Web Control Panel Application");
-
+				// init
 				WebAppConfigEntry app = Metabase.GetWebAppEntry(appId);
 				app.PhysicalDirectory = path;
 				app.ListenAddresses.AddAddresses(new ListenAddress(port));
 				app.ApplicationName = "NooLite Web Control Panel";
+
+				// register webapp
 				Metabase.RegisterApplication(RuntimeVersion.AspNet_4, false, app, new AppShortcut[0]);
-
-
-
-				session.Log("End Configure NooLite Web Control Panel Application");
 			}
 			catch (Exception ex)
 			{
-				session.Log("ERROR in Configure NooLite Web Control Panel Application: {0}", ex.ToString());
+				session.Log("ERROR in configure web application: {0}", ex.ToString());
 				return ActionResult.Failure;
 			}
 
+			session.Log("End configure web application");
+			return ActionResult.Success;
+		}		
+		
+		[CustomAction]
+		public static ActionResult UnRegisterApplication(Session session)
+		{
+			Debugger.Launch();
+			session.Log("Begin unregister web application");
+
+			try
+			{
+				Guid appId = new Guid(session["APP_ID"]);
+				Metabase.UnregisterApplication(appId);
+			}
+			catch (Exception ex)
+			{
+				session.Log("ERROR in unregister web application: {0}", ex.ToString());
+				return ActionResult.Failure;
+			}
+
+			session.Log("End unregister web application");
 			return ActionResult.Success;
 		}
 	}
