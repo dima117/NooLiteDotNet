0d0fdf961c05c733232858ca8a50d3cdd3e3de12
dima117a <dima117a@97b3ecbd-a1e6-0508-9bb6-6bfb78539658>
2013-08-30 20:24:42 +0000

    - получение данных о последней команде RX;

--- trunk/ThinkingHome.NooLite/RX1164Adapter.cs
+++ trunk/ThinkingHome.NooLite/RX1164Adapter.cs
@@ -1,4 +1,6 @@
-﻿namespace ThinkingHome.NooLite
+﻿using System;
+
+namespace ThinkingHome.NooLite
 {
 	public class RX1164Adapter : BaseAdapter
 	{
@@ -7,20 +9,45 @@
 			get { return 0x05DC; }
 		}
 
-		public byte[] ReadLatestCommand()
+		public ReceivedCommandData ReadLatestCommand()
 		{
 			byte[] buf;
 			device.ReadFeatureData(out buf);
 
-			return buf;
+			var result = new ReceivedCommandData();
+
+			var flags = buf[1];
+			result.Binding = (flags & 0x80) > 0;	// 6й бит 1-го байта
+			result.ToggleFlag = (flags & 0x40) > 0;	// 7й бит 1-го байта
+
+			result.Channel = buf[2];
+			result.Cmd = buf[3];
+
+			switch ((AdapterCommandFormat)buf[4])
+			{
+				case AdapterCommandFormat.OneByteData:
+					result.Data = new[] { buf[5] };
+					break;
+				case AdapterCommandFormat.FourByteData:
+					result.Data = new[] { buf[5], buf[6], buf[7], buf[8] };
+					break;
+				case AdapterCommandFormat.Undefined:
+				case AdapterCommandFormat.LED:
+					result.Data = new byte[0];
+					break;
+				default:
+					throw new Exception("unknown command format");
+			}
+
+			return result;
 		}
 
 		public void SendCommand(RX1164Command cmd, byte channel = 0)
 		{
 			var data = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-			data[1] = (byte)cmd;	
-			data[2] = channel;		
+			data[1] = (byte)cmd;
+			data[2] = channel;
 
 			device.WriteFeatureData(data);
 			System.Threading.Thread.Sleep(200);
---
+++ trunk/ThinkingHome.NooLite/ReceivedCommandData.cs
@@ -0,0 +1,15 @@
+﻿namespace ThinkingHome.NooLite
+{
+	public class ReceivedCommandData
+	{
+		public bool ToggleFlag { get; set; }
+
+		public bool Binding { get; set; }
+
+		public byte Cmd { get; set; }
+
+		public byte Channel { get; set; }
+
+		public byte[] Data { get; set; }
+	}
+}
